205280d47e661258951b912965839ef560a1b8db
sinsu <sinsu19012@gmail.com>
2022-10-27 15:52:41 +0900

    ✨[추가] : web3 연결

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express=require("express");
 const app=express();
 const port=5000;
 const {conn}=require("./connect/db.js");
+const {web3}=require("./connect/web3.js");
 
 app.listen(port,()=>{
     console.log("서버가 정상적으로 실행되었습니다.");
