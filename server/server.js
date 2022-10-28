e0e99df055753955e95a34ca67778892a76adc48
sinsu <sinsu19012@gmail.com>
2022-10-28 19:44:17 +0900

    🎨[변경]: router 구조 변경, Sequelize ORM 적용

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express=require("express");
 const app=express();
 const port=5000;
-const {conn}=require("./connect/db.js");
 const {web3}=require("./connect/web3.js");
 
+
+app.use(express.json({limit: '100mb'}));
+app.use(express.urlencoded({limit: '100mb', extended: false}));
 app.listen(port,()=>{
     console.log("서버가 정상적으로 실행되었습니다.");
 });
@@ -11,7 +13,6 @@ app.get("/",(req,rep)=>{
     rep.send("성공입니다.");
 });
 
-conn.connect((err)=>{
-    if(err) console.log(err);
-    console.log("connect");
-})
+require("./routes/index.js")(app);
+
+
