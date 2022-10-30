95e846f7a146a74829c42144edb4c48283f19dce
sinsu <sinsu19012@gmail.com>
2022-10-30 22:03:33 +0900

    🐛Fix:axios 에러 해결

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express=require("express");
 const app=express();
+const cors=require("cors");
 const port=5000;
 const {web3}=require("./connect/web3.js");
 
-
+app.use(cors());
 app.use(express.json({limit: '100mb'}));
 app.use(express.urlencoded({limit: '100mb', extended: false}));
 app.listen(port,()=>{
