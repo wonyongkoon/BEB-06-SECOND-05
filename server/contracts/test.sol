3c456a0b2e322ce60963b6bebd1fab93a0490b97
smkang2 <rkdtjrals2@naver.com>
2022-11-01 15:33:21 +0900

    smart contract

---
+++ server/contracts/test.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract tranferERC20 {
+    address public owner;
+    uint256 public balance;
+
+    event TransferReceived(address _from, uint _amount);
+    event TransferSent(address _from, address _destAddr, uint _amount);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    receive() payable external {
+        balance += msg.value;
+        emit TransferReceived(msg.sender, msg.value);
+    }
+
+    function withdraw(uint amount, address payable destAddr) public {
+        require(msg.sender == owner, "Only owner can withdraw fund");
+        require(amount <= balance, "insufficient funds");
+
+        destAddr.transfer(amount);
+        balance -= amount;
+        emit TransferSent(msg.sender, destAddr, amount);
+    }
+
+    function tranfertokens(IERC20 token, address to, uint256 amount) public {
+        require(msg.sender == owner, "Only owner can withdraw funds");
+        uint256 erc20balance = token.balanceOf(address(this));
+        require(amount <= erc20balance, "balance is low");
+        token.transfer(to, amount);
+        emit TransferSent(msg.sender, to, amount);
+    }
+}
