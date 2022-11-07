5519de6e45658d331ed546be7451aaea79489a5f
yoo-tonec <yooyoosoo8989@gmail.com>
2022-11-07 10:51:34 +0900

    💄[디자인] side,right,header,footer

--- client/src/components/sidebar/Sidebar.js
+++ client/src/components/sidebar/Sidebar.js
@@ -1,70 +1,85 @@
 import React from "react";
 import "./sidebar.css";
-import {RssFeed, Chat, PlayCircleFilledOutlined, Group, Bookmark, WorkOutline, Event, School} from "@material-ui/icons";
-import {Link} from "react-router-dom";
+import image from "../rightbar/iu2.jpeg"
 
 
 const Sidebar = () => {
-    return (
-        <div className="sidebar">
 
-            <div className="sidebarWrapper">
-
-                    <ul className="sidebarList">
-                          <li className="sidebarListItem">
-                            <Group className="sidebarIcon"/>
-                            <span className="sidebarListItemText">Friends</span>
-                          </li>
-
-                          <li className="sidebarListItem">
-                            <RssFeed className="sidebarIcon"/>
-                            <span className="sidebarListItemText">Feed</span>
-                          </li>
-
-                          <li className="sidebarListItem">
-                            <Chat className="sidebarIcon"/>
-                            <span className="sidebarListItemText">Messages</span>
-                          </li>
-
-                          <li className="sidebarListItem">
-                            <PlayCircleFilledOutlined className="sidebarIcon"/>
-                            <span className="sidebarListItemText">Videos</span>
-                          </li>
-
-                          <li className="sidebarListItem">
-                            <Bookmark className="sidebarIcon"/>
-                            <span className="sidebarListItemText">Bookmarks</span>
-                          </li>
-
-                          <li className="sidebarListItem">
-                            <WorkOutline className="sidebarIcon"/>
-                            <span className="sidebarListItemText">Jobs</span>
-                          </li>
-
-                          <li className="sidebarListItem">
-                            <School className="sidebarIcon"/>
-                            <span className="sidebarListItemText">Courses</span>
-                          </li>
-
-                          <li className="sidebarListItem">
-                            <Event className="sidebarIcon"/>
-                            <span className="sidebarListItemText">Now</span>
-                          </li>
-
-                          
-
-                    </ul>
-
-                    {/* <button className="sidebarButton">Show More</button> */}
-
-                    <hr className="sidebarHr"></hr>
-
-                    {/* <span className="sidebarFriendList">Friends</span> */}
-
-            </div>
-
+  return (
+    <div className="sidebar">
+    <div className="sidebarWrapper ">
+      <div >
+        <div >
+          <img
+            alt="logo"
+            style={{
+              height: 150,
+              width: 150,
+              border: "4px solid #fff",
+              borderRadius: "50%"
+            }}
+            src={image}
+          />
         </div>
-    )
+        <div>
+          <h4 className="sideTitle">yoo</h4>
+        </div>
+          <div className="sideText">testyoo</div>
+          <div className="sideText">yooyoosoo8989@gmail.com</div>
+        </div>
+      </div>
+      </div>
+  )
+    {/* // return (
+    //     <div className="sidebar">
+
+    //         <div className="sidebarWrapper">
+
+    //                 <ul className="sidebarList">
+    //                       <li className="sidebarListItem">
+    //                         <Group className="sidebarIcon"/>
+    //                         <span className="sidebarListItemText">Friends</span>
+    //                       </li>
+
+    //                       <li className="sidebarListItem">
+    //                         <RssFeed className="sidebarIcon"/>
+    //                         <span className="sidebarListItemText">Feed</span>
+    //                       </li>
+
+    //                       <li className="sidebarListItem">
+    //                         <Chat className="sidebarIcon"/>
+    //                         <span className="sidebarListItemText">Messages</span>
+    //                       </li>
+
+    //                       <li className="sidebarListItem">
+    //                         <PlayCircleFilledOutlined className="sidebarIcon"/>
+    //                         <span className="sidebarListItemText">Videos</span>
+    //                       </li>
+
+    //                       <li className="sidebarListItem">
+    //                         <Bookmark className="sidebarIcon"/>
+    //                         <span className="sidebarListItemText">Bookmarks</span>
+    //                       </li>
+
+    //                       <li className="sidebarListItem">
+    //                         <WorkOutline className="sidebarIcon"/>
+    //                         <span className="sidebarListItemText">Jobs</span>
+    //                       </li>
+
+    //                       <li className="sidebarListItem">
+    //                         <School className="sidebarIcon"/>
+    //                         <span className="sidebarListItemText">Courses</span>
+    //                       </li>
+
+    //                       <li className="sidebarListItem">
+    //                         <Event className="sidebarIcon"/>
+    //                         <span className="sidebarListItemText">Now</span>
+    //                       </li>
+    //                 </ul>
+    //         </div>
+
+    //     </div>
+    // ) */}
 }
 
 export default Sidebar
